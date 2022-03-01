a4cac665e5e6b273d33a984db655a6ae84594cdc
Jefferson Taylor <jctaylor2nd@gmail.com>
2022-03-01 16:43:08 -0500

    added example images, different pages (home, about, charts)

--- SCTLDoc/charts/views.py
+++ SCTLDoc/charts/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class AboutChartsView(TemplateView):
+    template_name = "charts.html"
