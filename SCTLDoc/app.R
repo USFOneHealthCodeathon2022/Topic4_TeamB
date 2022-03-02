36d1ad0a6fd1b1733ebc2b464d36bbc6080beac1
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2022-03-02 10:37:50 -0500

    app actually works

--- SCTLDoc/app.R
+++ SCTLDoc/app.R
@@ -2,17 +2,19 @@ library(shiny)
 library(leaflet)
 library(phyloseq)
 library(dada2)
+library(microbiome)
+library(ggplot2)
 
 ## contains physeq_16S; a phyloseq object with count table, metadata, and taxonomy information bundled together
 load('reference_data.RData')
 
-subset_samples <- function(physeq_16S, criteria) {
+custom_subset_samples <- function(physeq_16S, criteria) {
   ## create new set of samples for plotting based on interactive selection criteria
   subset_samples(physeq_16S, sample_type == "TissueSlurry" | sample_type == "Mucus" | sample_type == "TissueSlurry_Skleton" |
                    sample_type=="Seawater" | sample_type=="Sediment")
 }
 
-plot_map <- function(ps) {
+my_plot_map <- function(ps) {
   ## generate plot for panel 1
   ## https://rstudio.github.io/leaflet/shiny.html
   filtDat <- sample_data(ps)
@@ -28,23 +30,23 @@ plot_map <- function(ps) {
   ##data = map_click(),
 }
 
-plot_taxonomy <- function(ps) {
+my_plot_taxonomy <- function(ps) {
   ## generate plot for panel 2
   sum_ps <- ps  %>%
     tax_glom(taxrank = "Family") %>%
     transform_sample_counts(function(x) {x/sum(x)}) %>% # Transform to rel. abundance
     psmelt()
-  
-  sum_ps$tissue_type <- factor(sum_ps$tissue_type, 
+
+  sum_ps$tissue_type <- factor(sum_ps$tissue_type,
                                levels = c("AH","DU", "DL"))
-  ggplot(subset(sum_ps, Abundance>0.5 ), 
-         aes(x=sample_type, y=Abundance, fill=Order)) + 
+  ggplot(subset(sum_ps, Abundance>0.5 ),
+         aes(x=sample_type, y=Abundance, fill=Order)) +
     geom_bar(stat="identity", position="fill") +
     facet_grid(tissue_type~., scales = "free") +
     scale_fill_manual(values=c("#56B4E9","#CBD588","#5F7FC7", "orange","#DA5724","#CD9BCD",
-                               "gray80", "#AD6F3B", "#673770","#D14285", "#652926","#8569D5", 
+                               "gray80", "#AD6F3B", "#673770","#D14285", "#652926","#8569D5",
                                "#5E738F","#D1A33D", "#8A7C64","lightsalmon","aquamarine4",
-                               "lightblue4", "lightpink", "ivory4","royalblue4", "darkorchid", 
+                               "lightblue4", "lightpink", "ivory4","royalblue4", "darkorchid",
                                "palevioletred1", "#56B4E9","#CBD588","yellow2","#5F7FC7", "orange","#DA5724",
                                "#CD9BCD", "gray80",
                                "#AD6F3B", "#673770","#D14285", "#652926","#8569D5", "#5E738F",
@@ -54,7 +56,7 @@ plot_taxonomy <- function(ps) {
     theme_classic()
 }
 
-plot_ordination <- function(ps) {
+my_plot_ordination <- function(ps) {
   ## generate plot for panel 3
   ps_clr <- microbiome::transform(ps, 'clr')
   psr_clr.ord <- ordinate(ps_clr, "RDA", "euclidean")
@@ -70,15 +72,15 @@ plot_ordination <- function(ps) {
     theme(axis.text.y = element_text(size = 6)) +
     scale_color_manual(values=c("#007f00", "Navy", "#800020"),
                        labels=c(AH="Apparently Healthy",
-                                DU="Diseased Unaffected", 
+                                DU="Diseased Unaffected",
                                 DL="Diseased Lesion")) +
     stat_ellipse()
 }
 
-plot_boxes <- function() {
+my_plot_boxes <- function(dat) {
   ## generate plot for panel 4
-  
-  
+  plot(c(0,1,2),c(5,4,6))
+
 }
 
 # increase max R-Shiny user-input file size from 5 MB to 3 GB
@@ -109,17 +111,17 @@ ui <- fluidPage(
 server <- function(input, output) {
   p <- reactiveValues(panel1=NULL,panel2=NULL,panel3=NULL,panel4=NULL)
   observeEvent(input$user_disease, {
-    #user_counts <- makeSequenceTable(dada(input$user_reads$datapath, pool='pseudo', selfConsist=TRUE, err=NULL))
-    #user_phyloseq <- phyloseq(otu_table(user_counts, taxa_are_rows = T), sample_data(read.table(input$user_disease$datapath)))
-    #merged_data <- merge_phyloseq(physeq_16S,user_phyloseq)
-    merged_data <- physeq_16S
+    user_counts <- makeSequenceTable(dada(input$user_reads$datapath, pool='pseudo', selfConsist=TRUE, err=NULL))
+    user_phyloseq <- phyloseq(otu_table(user_counts, taxa_are_rows = T), sample_data(read.table(input$user_disease$datapath)))
+    merged_data <- merge_phyloseq(ps,user_phyloseq)
+    merged_data <- ps
     subsetted_data <- merged_data
     #criteria <- list() ## create a set of subsetting criteria based on user input
-    #subsetted_data <- subset_samples(merged_data, criteria)
-    p$panel1 <- plot_map(merged_data)
-    p$panel2 <- plot_taxonomy(subsetted_data)
-    p$panel3 <- plot_ordination(subsetted_data)
-    p$panel4 <- plot_boxes(subsetted_data)
+    #subsetted_data <- custom_subset_samples(merged_data, criteria)
+    p$panel1 <- my_plot_map(merged_data)
+    p$panel2 <- my_plot_taxonomy(subsetted_data)
+    p$panel3 <- my_plot_ordination(subsetted_data)
+    p$panel4 <- my_plot_boxes(subsetted_data)
   })
   output$panel1 <- renderLeaflet(if(is.null(p$panel1)) return() else {p$panel1})
   output$panel2 <- renderPlot(if(is.null(p$panel2)) return() else {p$panel2})
--- SCTLDoc/preprocess.r
+++ SCTLDoc/preprocess.r
@@ -5,14 +5,15 @@ library(tibble)
 library(ggplot2)
 library(phyloseq)
 
-# Read qza files 
+# Read qza files
 #Import count table
 ASV_table <- read_qza("input_data/tableV_BacArc_99_SCTLD.qza")
-ASV_table <- ASV_table$data 
+ASV_table <- ASV_table$data
+ASV_table <- ASV_table[sample(1:nrow(ASV_table),1000),]
 #Import taxon
 taxa_16S <- read_qza("input_data/taxaVsearch_rep-seqs-dn-99_SCTLD.qza")
 taxa_16S <- taxa_16S$data %>% as_tibble() %>% separate(Taxon, sep=";",
-                                                       c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")) 
+                                                       c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"))
 # Convert taxonomy info to data frame with correct taxonomy labels
 taxa_16S <- taxa_16S[-1,]
 
@@ -25,4 +26,8 @@ physeq_16S <- phyloseq(otu_table(ASV_table, taxa_are_rows= T),
                        tax_table(as.data.frame(taxa_16S) %>% column_to_rownames("Feature.ID") %>%
                                    as.matrix()), sample_data(meta_data))
 
-save(physeq_16S, file='reference_data.RData')
+ps <- subset_samples(physeq_16S, sample_type == "TissueSlurry" | sample_type == "Mucus" | sample_type == "TissueSlurry_Skleton" |
+                     sample_type=="Seawater" | sample_type=="Sediment")
+ps <- filter_taxa(ps, function(x) sum(x > 20) > (0.01*length(x)), TRUE)
+
+save(ps, file='SCTLDoc/reference_data.RData')
